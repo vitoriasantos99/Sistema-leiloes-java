690db79d5620b9f53fdbbf8bfa307ff93e7c9259
vitoriasantos99 <vitoriasantos.cristina@hotmail.com>
2026-02-03 07:42:18 -0300

    Projeto funcionando base

---
+++ README.md
@@ -0,0 +1,32 @@
+\# Sistema de Leilões
+
+
+
+Projeto desenvolvido em Java com integração ao banco de dados MySQL.
+
+O sistema permite o cadastro e a consulta de produtos destinados a leilão.
+
+
+
+\## Funcionalidades
+
+\- Cadastro de produtos
+
+\- Listagem de produtos cadastrados
+
+\- Integração com banco de dados
+
+
+
+\## Tecnologias utilizadas
+
+\- Java
+
+\- MySQL
+
+\- Git
+
+\- GitHub
+
+
+
---
+++ src/ProdutosDTO.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Adm
+ */
+public class ProdutosDTO {
+    private Integer id;
+    private String nome;
+    private Double valor;
+    private String status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+}
+
+
