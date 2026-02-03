64aeff8a221084c76caf9f61273c1293d8d7e3c9
vitoriasantos99 <vitoriasantos.cristina@hotmail.com>
2026-02-03 07:48:19 -0300

    listagem de produtos funcionando

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -20,6 +20,7 @@ public class listagemVIEW extends javax.swing.JFrame {
     public listagemVIEW() {
         initComponents();
         listarProdutos();
+        // Commit 2 - listagem de produtos funcionando
     }
 
     /**
