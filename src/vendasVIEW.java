ac8beb7206bf5d03a5ad3aedb9f2b65b4f2a0e9c
vitoriasantos99 <vitoriasantos.cristina@hotmail.com>
2026-02-03 07:53:53 -0300

    criada a tela de vendas

--- src/vendasVIEW.java
+++ src/vendasVIEW.java
@@ -36,6 +36,7 @@ public class vendasVIEW extends javax.swing.JFrame {
     public vendasVIEW() {
         initComponents();
         listarVendas();
+        // Commit 4 - tela de vendas criada
     }
 
     /**
